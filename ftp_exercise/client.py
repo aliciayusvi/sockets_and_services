f8b225f5ea708a442ebd19f7473f1e26ceea3414
aliciayusvi <aliciayusvi@gmail.com>
2024-03-05 22:23:57 +0000

    multi threading exercise

---
+++ ftp_exercise/client.py
@@ -0,0 +1,60 @@
+import logging
+import argparse
+
+import socket
+import logging
+from typing import Self
+
+logger = logging.getLogger("AliciaClient")
+
+
+class Client:
+    def __init__(self, host: str = "server", port: int = 4444) -> None:
+        self.host = host
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect(self) -> None:
+        service_address = (self.host, self.port)
+        logger.info(f"Conectando a {self.host}:{self.port}")
+        self.socket.connect(service_address)
+
+    def send(self, data: bytes) -> bytes:
+        self.socket.sendall(data)
+
+    def receive(self, buffer_size: int = 1024) -> bytes:
+        return self.socket.recv(buffer_size)
+
+    def close(self) -> None:
+        logger.info("Cerrando conexión")
+        self.socket.close()
+
+    def __enter__(self) -> Self:
+        self.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        self.close()
+
+
+
+class ClientTest(Client):
+    def get_time(self):
+        self.socket.sendall(b"message")
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    parser = argparse.ArgumentParser(description="Cliente de Alicia")
+    parser.add_argument("--address", "-a", default="0.0.0.0")
+    parser.add_argument("--port", "-p", default=4444, type=int)
+    args = parser.parse_args()
+
+    with ClientTest(host=args.address, port=args.port) as client:
+        while (message := input("mensaje (vacío para salir)? ")):
+            client.send(message.encode())
+
+
+if __name__ == "__main__":
+    main()
